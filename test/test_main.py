4ac0c2661b3a21b52bae60366faf1837b38a2c5b
Mim0410 <mim.humira.1087@gmail.com>
2026-09-16 16:00:11 +0600

    Fix pytest module import path

--- test/test_main.py
+++ test/test_main.py
@@ -1,3 +1,8 @@
+import sys
+import os
+sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+
+from src.main import api, tasks, Task  
 from fastapi.testclient import TestClient
 from src.main import api, tasks, Task
 
